74e8f2a6d14d7f70b46488025f710ff3c3bbeba2
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2024-01-13 03:46:07 +0530

    Responsive Layout

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,11 @@
 const Comment = ({ name, email, body }) => {
   return (
-    <div className="border border-gray-200 rounded-lg py-1 px-3 m-1 w-[24%] h-[200px]">
-      <div className="mb-3">
+    <div className="border border-gray-200 rounded-lg py-1 px-3 m-1 lg:w-[24%] lg:min-h-[250px]">
+      <div className="mb-3 p-1">
         <div className=" text-black font-bold text-md">{name}</div>
         <div className=" text-gray-400 text-sm px-2">{email}</div>
       </div>
-      <div className="text-justify">{body}</div>
+      <div className="text-justify p-1">{body}</div>
     </div>
   );
 };
--- src/components/ShimmerCard.jsx
+++ src/components/ShimmerCard.jsx
@@ -2,7 +2,7 @@ import "../App.css";
 
 const ShimmerCard = () => {
   return (
-    <div className="border border-gray-200 rounded-lg py-1 px-3 m-1 w-[24%] bg-slate-200 h-[200px] animate-pulse" />
+    <div className="border border-gray-200 rounded-lg py-1 px-3 m-1 w-[100%] min-h-[200px] lg:w-[24%] lg:min-h-[250px] bg-slate-200 animate-pulse" />
   );
 };
 
