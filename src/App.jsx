85f0d5f29c54540e6541749dfce130962e6e585a
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2024-01-28 02:15:02 +0530

    Counting with Context API - CreateContext and useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./components/Counter";
 import CommentSection from "./components/CommentSection";
+import { createContext, useState } from "react";
+
+export const counterContext = createContext();
 
 function App() {
+  const [count, setCount] = useState(101);
   return (
-    <div className="p-0 m-0">
-      <h1 className="flex justify-center text-lg font-extrabold m-3 p-3 bg-purple-950 text-white ">
-        React 101
-      </h1>
-      <Counter initialValue={25} />
-      <CommentSection />
-      {/* <Comment name={"id labore ex et quam laborum"} email={"Eliseo@gardner.biz"} body={"laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"}/> */}
-    </div>
+    <counterContext.Provider value={{ count, setCount }}>
+      <div className="p-0 m-0">
+        <h1 className="flex justify-center text-lg font-extrabold m-3 p-3 bg-purple-950 text-white ">
+          React 101
+        </h1>
+        <Counter initialValue={25} />
+        <CommentSection />
+        {/* <Comment name={"id labore ex et quam laborum"} email={"Eliseo@gardner.biz"} body={"laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"}/> */}
+      </div>
+    </counterContext.Provider>
   );
 }
 
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { counterContext } from "../App";
 
-const Counter = ({ initialValue }) => {
-    const [count, setCount] = useState(initialValue);
-
-    return (
-        <>
-            <div className="flex justify-center">
-                    <button className="m-1 px-2 py-2 bg-purple-600 rounded border-black text-white hover:bg-purple-800" onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
-                    <span className="m-1 p-2 font-bold">{String(count)}</span>
-                    <button className="m-1 px-2 py-2 bg-purple-600 rounded border-black text-white hover:bg-purple-800" onClick={() => setCount((prev) => prev + 1)}>Increment</button>
-            </div>
-        </>
-    );
-}
+const Counter = () => {
+  const { count, setCount } = useContext(counterContext);
+  return (
+    <>
+      <div className="flex justify-center">
+        <button
+          className="m-1 px-2 py-2 bg-purple-600 rounded border-black text-white hover:bg-purple-800"
+          onClick={() => setCount((prev) => prev - 1)}
+        >
+          Decrement
+        </button>
+        <span className="m-1 p-2 font-bold">{String(count)}</span>
+        <button
+          className="m-1 px-2 py-2 bg-purple-600 rounded border-black text-white hover:bg-purple-800"
+          onClick={() => setCount((prev) => prev + 1)}
+        >
+          Increment
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Counter;
